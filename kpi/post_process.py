52300958d8ddeb4c58c613b65611494297789309
hrbyyy <31474708+hrbyyy@users.noreply.github.com>
2022-01-27 21:30:05 +0800

    Update post_process.py

--- kpi/post_process.py
+++ kpi/post_process.py
@@ -1,6 +1,5 @@
 # --coding:utf-8--
-# 按hw为分组，不同fsize时固定层数，并调整dilationrate
-# 采用的是将ts变化成回归形式的数据集，并按时间先后划分train-val-test
+
 import sys
 import torch
 import torch.nn as nn
@@ -24,42 +23,33 @@ from functools import reduce
 from torch.nn import BatchNorm1d
 import time
 
-# import score_LSTM
-# from synthetic_data.big_sin import score_LSTM
-# from synthetic_data import preprocess
+
 import score_uts
 
 
-# import new_score_func
 
 
 def combine_para(lists):
     def my_fun(list1, list2):
         return [str(i) + ',' + str(j) for i in list1 for j in
-                list2]  # 采用str形式，后续利用split得到每个元素值，如果此处包装成[i，j],则会变成[[i,j],k]形式
+                list2]  
 
     return reduce(my_fun, lists)
 
 
-window = 60  # 预测步长,30,40
-# hw=10        #288
-# num_sim=1  #k_sim中k的数目
+window = 60  
+
 
 repeats = 5
-# k = window // 2  # 针对window10
-k=7 #point_adjust对应的delay_tolerance  #针对window30+
+
+
 rate_threshold = 0.1
-# p0 = '/mnt/A/PycharmProject/wavelet_rec/kpi/ablation/'
-# p0 = p0 + 'temporal_conv/'+'window'+str(window)+'/'
-# p0=p0+"RMSprop/learning_rate0.001/"
-# p0='/mnt/A/PycharmProject/wavelet_rec/kpi/conv_inception/window60/haar/level2/RMSprop1e-3/'
-# p0='/mnt/A/PycharmProject/wavelet_rec/kpi/conv_temporal_freq/window60/haar/level2/RMSprop/learning_rate0.001/'
-#p0 = '/mnt/A/PycharmProject/wavelet_rec/kpi/ablation/temporal_conv/window10/RMSprop/learning_rate0.001/'
+
 p0="/mnt/A/PycharmProject/wavelet_rec/kpi/double_inputs/conv/window60/haar/level2/RMSprop/learning_rate0.0005/"
 
-# 单独保存每个ts_entity的结果
 
-def compute_oldmetric(true_labels, pred_labels, outp, indicator):  # 传入real_label和predict_label
+
+def compute_oldmetric(true_labels, pred_labels, outp, indicator):  
 
     pred_labels = np.reshape(pred_labels, newshape=(len(pred_labels), 1))
     true_labels = np.reshape(true_labels, newshape=(len(true_labels), 1))
@@ -78,7 +68,7 @@ def compute_oldmetric(true_labels, pred_labels, outp, indicator):  # 传入real_
     else:
         P = TP / (TP + FP)
         R = TP / (TP + FN)
-        F1 = 2 * P * R / (P + R)  # 尝试改用Fb(bata,定bata值)
+        F1 = 2 * P * R / (P + R) 
         tpr = TP / (TP + FN)
     if FP == 0:
         fpr = 0
@@ -88,7 +78,7 @@ def compute_oldmetric(true_labels, pred_labels, outp, indicator):  # 传入real_
         rpr = 'inf'
     else:
         rpr = tpr / fpr
-    # 用list of dict 初始化df,每个dict是一个记录
+   
     df_out = pd.DataFrame(
         [{'P': P, "R": R, 'F1': F1, 'FP rate': fpr, 'relative positive ratio': rpr}])
     df_out.to_csv(outp + indicator + 'pred_metrics.csv')
@@ -96,24 +86,23 @@ def compute_oldmetric(true_labels, pred_labels, outp, indicator):  # 传入real_
 
 
 def seqlevel_threshold(seqas_array, val_label, outp,
-                       score_indicator):  # 定义函数求threshold      传入VN2和VA的anomaly score(合为一个列表),以及val_y(list,真实0，1label,而非regression值)
-    # 原求point_score方法错误，应该用多个时刻的score均值，而非和label取法一致，将首列和末行结果拼接，只取一个时刻的值
+                       score_indicator):
+  
 
     candidates = sorted(seqas_array, reverse=True)
-    # for i in range(len(val_score_list) - 1):
-    #     candidates.append((val_score_list[i] + val_score_list[i + 1]) / 2)
+  
     index = -1
     fmax = -1
-    # score_array=np.asarray(sequence_score).reshape((len(sequence_score), 1))
+    
     score_array = seqas_array.astype(int)
-    # val_label=np.asarray(val_label).reshape((len(val_label), 1))
+   
     val_label = val_label.astype(int)
 
-    # 补充val_y变成sequence_label
+  
 
     for j in range(len(candidates)):
         pred_label = np.where(score_array >= candidates[j], 1, 0)
-        # 补充pred_label变成sequence label
+       
         pred_label = pred_label.astype(int)
         data = np.hstack((pred_label, val_label))
         df = pd.DataFrame(data=data, columns=['pred', 'real'])
@@ -126,7 +115,7 @@ def seqlevel_threshold(seqas_array, val_label, outp,
         else:
             P = TP / (TP + FP)
             R = TP / (TP + FN)
-            F1 = 2 * P * R / (P + R)  # 尝试改用Fb(bata,定bata值)
+            F1 = 2 * P * R / (P + R) 
             tpr = TP / (TP + FN)
         if FP == 0:
             fpr = 0
@@ -147,7 +136,7 @@ def seqlevel_threshold(seqas_array, val_label, outp,
 
 
 l = window // 2
-selected_ts = [16, 26, 27]  # 21,22, 25,28,29,15,17,19,21,12,
+selected_ts = [16, 26, 27] 
 
 
 def post_cal(ts_id, rootp, repeats):
@@ -162,7 +151,7 @@ def post_cal(ts_id, rootp, repeats):
 
         val2dict = pickle.load(open(outp + 'val2' + '_errslabels.pkl', 'rb'))
         val2_errvs = val2dict['error_vectors'].squeeze(axis=1)
-        val2_labels = val2dict['true_labels']  # 从dataloader拼接而得，已经越过初始hw点
+        val2_labels = val2dict['true_labels']  
 
         testdict = pickle.load(open(outp + 'test' + '_errslabels.pkl', 'rb'))
         test_errvs = testdict['error_vectors'].squeeze(axis=1)
@@ -175,20 +164,18 @@ def post_cal(ts_id, rootp, repeats):
         test_realseqs = testdict['real_seqs']
         # test_probs = testdict['predict_probs']
 
-        # 原sequence metric 和mocap统一
-        raw_val2as = np.abs(val2_realseqs - val2_predseqs).squeeze(axis=1)  # (b,f,t) f=1,需降维。
+        raw_val2as = np.abs(val2_realseqs - val2_predseqs).squeeze(axis=1)  # (b,f,t) f=1
         raw_testas = np.abs(test_realseqs - test_predseqs).squeeze(axis=1)
 
-        val2_seqas = raw_val2as.sum(axis=1).reshape(len(raw_val2as), 1)  # 保持二维形式，便于后续求metric
+        val2_seqas = raw_val2as.sum(axis=1).reshape(len(raw_val2as), 1)  
         test_seqas = raw_testas.sum(axis=1).reshape(len(raw_testas), 1)
       
-        # 对于kpi，要先将pointlabel化为seqlabel
         real_val2labels = score_uts.trans_reallabel(val2_labels, 1).reshape(len(val2_labels), 1)
         real_testlabels = score_uts.trans_reallabel(test_labels, 1).reshape(len(test_labels), 1)
 
         threshold = seqlevel_threshold(val2_seqas, real_val2labels, outp, 'MAE')
       
-        predict_seqlabel = np.where(test_seqas > threshold, 1, 0)  # 利用np.where,无需特意编写函数
+        predict_seqlabel = np.where(test_seqas > threshold, 1, 0) 
         # real_pointlabel = score_uts.true_pointlabel(test_labels)
         compute_oldmetric(real_testlabels, predict_seqlabel, outp, 'seqMAE')
       
