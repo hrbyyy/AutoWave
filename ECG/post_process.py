5f2762b4414e6d296ca367db56bdfd7d06f3cce6
hrbyyy <31474708+hrbyyy@users.noreply.github.com>
2023-02-14 20:49:42 +0800

    Update post_process.py

--- ECG/post_process.py
+++ ECG/post_process.py
@@ -7,10 +7,7 @@ import torch.nn as nn
 from torch.nn.utils import weight_norm
 import multiprocessing
 import os
-extra_path='/home/ices/PycharmProject/shape_sequence_kpi/uts/conditional_conv/model_combine/uts_WGAN_4score/'
-extra_path2='/home/ices/PycharmProject/shape_sequence_kpi/uts/conditional_conv/model_combine/uts_WGAN_4score/frequency_condition/'
-sys.path.append(extra_path)
-sys.path.append(extra_path2)
+
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
@@ -27,11 +24,9 @@ from functools import reduce
 from torch.nn import BatchNorm1d
 import time
 
-# import score_LSTM
-# from synthetic_data.big_sin import score_LSTM
-#from synthetic_data import preprocess
+
 import score_GAN_uts
-# import new_score_func
+
 
 
 def combine_para(lists):
@@ -50,9 +45,7 @@ p0='/mnt/A/PycharmProject/wavelet_rec/ecg/double_inputs/conv/haar/level4/adam10.
 
 def compute_oldmetric(true_labels,pred_labels,outp,indicator): 
 
-    # pred_labels = np.reshape(pred_labels,newshape=(len(pred_labels), 1))
-    # true_labels = np.reshape(true_labels,newshape=(len(true_labels), 1))
-
+   
     pred_labels, true_labels =pred_labels.astype(int),true_labels.astype(int)
 
 
@@ -88,13 +81,12 @@ def seqlevel_threshold(seqas_array, val_label, outp,score_indicator):
   
 
     candidates=sorted(seqas_array, reverse=True)
-    # for i in range(len(val_score_list) - 1):
-    #     candidates.append((val_score_list[i] + val_score_list[i + 1]) / 2)
+  
     index=-1
     fmax=-1
-    # score_array=np.asarray(sequence_score).reshape((len(sequence_score), 1))
+   
     score_array=seqas_array.astype(int)
-    # val_label=np.asarray(val_label).reshape((len(val_label), 1))
+    
     val_label=val_label.astype(int)
 
 
@@ -132,13 +124,12 @@ def seqlevel_threshold(seqas_array, val_label, outp,score_indicator):
     df_out.to_csv(outp+score_indicator+'_pointlevel_threshold.csv')
     return candidates[index]
 
-# l = pw // 2
+
 
 def post_cal(rootp,repeats):
 
     ppath=rootp
-    # ppath = rootp + 'encoder' + str(encoder_layer) + 'unit' + str(encoder_unit) + 'decoder' + str(decoder_layer) + 'unit' + str(decoder_unit) + "/"
-
+   
 
     for r in range(repeats):
         outp = ppath + str(r) + '/'
@@ -151,15 +142,15 @@ def post_cal(rootp,repeats):
         test_errvs=testdict['error_vectors'].squeeze(axis=1)
         test_labels = testdict['true_labels']
 
-        val2_predseqs = val2dict['rec_seqs']  # (b,t,f) f=1
+        val2_predseqs = val2dict['rec_seqs']  
         val2_realseqs = val2dict['real_seqs']
-        # val2_probs = val2dict['predict_probs']
+      
         test_predseqs = testdict['rec_seqs']
         test_realseqs = testdict['real_seqs']
-        # test_probs = testdict['predict_probs']
+    
 
        
-        raw_val2as = np.abs(val2_realseqs - val2_predseqs).squeeze(axis=1)  # (b,f,t) f=1
+        raw_val2as = np.abs(val2_realseqs - val2_predseqs).squeeze(axis=1)  
         raw_testas = np.abs(test_realseqs - test_predseqs).squeeze(axis=1)
         val2_seqas=raw_val2as.sum(axis=1,keepdims=True)  
         test_seqas=raw_testas.sum(axis=1,keepdims=True)
