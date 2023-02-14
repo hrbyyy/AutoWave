c28bda21cc8e0e3ddf17941b4c843035af2ec733
hrbyyy <31474708+hrbyyy@users.noreply.github.com>
2023-02-14 20:38:22 +0800

    Update main_A2.py

--- A2/main_A2.py
+++ A2/main_A2.py
@@ -13,14 +13,14 @@ fatherpath = os.path.split(curPath)[0]
 grandpath=os.path.split(fatherpath)[0]
 greatgrandpath=os.path.split(grandpath)[0]
 gggpath=os.path.split(greatgrandpath)[0]
-# extra_path='/home/ices/PycharmProject/shape_sequence_kpi/uts/conditional_conv/model_combine/'
+
 sys.path.append(fatherpath)
 sys.path.append(curPath)
 sys.path.append(grandpath)
 sys.path.append(greatgrandpath)
 sys.path.append(gggpath)
 sys.path.append(os.path.split(gggpath)[0])
-# sys.path.append(extra_path)
+
 import numpy as np
 import pandas as pd
 import torch.optim as optim
@@ -58,13 +58,12 @@ def form_dataloader(batchsize,inpath):
     val1_x = torch.from_numpy(val1x_new).type(torch.FloatTensor)
     val2_x = torch.from_numpy(val2x_new).type(torch.FloatTensor)
     test_x = torch.from_numpy(testx_new).type(torch.FloatTensor)
-    # print(train_x.shape)  (b,f=2,t)
-    # exit()
+ 
     torch_trainset = Data.TensorDataset(train_x, train_x)
    
     trainloader = Data.DataLoader(
-        dataset=torch_trainset,  # torch TensorDataset format
-        batch_size=batchsize,  # mini batch size
+        dataset=torch_trainset,  
+        batch_size=batchsize,  
         shuffle=True  
     )
     val1data = Data.TensorDataset(val1_x, val1_x)
@@ -73,15 +72,7 @@ def form_dataloader(batchsize,inpath):
     val2loader = Data.DataLoader(dataset=val2data, shuffle=False, batch_size=batchsize)
     testdata = Data.TensorDataset(test_x, test_x)
     testloader = Data.DataLoader(dataset=testdata, shuffle=False, batch_size=batchsize)
-    # with open(inpath + 'trainloader.pkl', 'wb') as f7:
-    #     pickle.dump(trainloader, f7, protocol=4)
-    # with open(inpath +  'val1loader.pkl', 'wb') as f8:
-    #     pickle.dump(val1loader, f8, protocol=4)
-    # with open(inpath +  'val2loader.pkl', 'wb') as f9:
-    #     pickle.dump(val2loader, f9, protocol=4)
-    # with open(inpath +  'testloader.pkl', 'wb') as f10:
-    #     pickle.dump(testloader, f10, protocol=4)
-    # return  train_x,val1_x,val2_x,test_x
+   
     return trainloader,val1loader,val2loader,testloader
 
 class Options:
@@ -96,20 +87,20 @@ class Options:
 if __name__ == "__main__":
 
     repeats = 5
-    epoches = 200  #
+    epoches = 200  
 
     d_clip = 0.01
 
     os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    #
+  
 
     batchsize = 64
 
 
-    patience = 20  #
-    threshold = 2  # 
+    patience = 20 
+    threshold = 2  
 
 
     window =4
@@ -117,12 +108,11 @@ if __name__ == "__main__":
     datapath=datapath+'window'+str(window)+'/'
     wavelet_name = 'haar'
     wavelet = pywt.Wavelet('haar')
-    wavelet_len = wavelet.dec_len  # 2
+    wavelet_len = wavelet.dec_len
 
-    # print(len(morl))
-    # n_level = pywt.dwt_max_level(window, wavelet.dec_len)  # 6
+   
 
-    n_level=2 #
+    n_level=2 
     xfm = DWT1DForward(J=n_level,wave='haar').cuda()
     ifm=DWT1DInverse(wave='haar').cuda()
 
@@ -140,15 +130,9 @@ if __name__ == "__main__":
     len_list=[cur_len]+len_list    
     total_len+=cur_len   
     print(total_len)   
-    # len_list.append(cur_len)  
-    # len_list.reverse()  
-
-    # input_feature=len(scale_list)
-    # hidden_units = 64
-    # n_layer = 1
-    # dropout = 0  #
+  
     ts_cin=1
-    freq_cout=4   #freq_tuner中对应的c_out
+    freq_cout=4   
     nc=1
     lr=1e-3
   
@@ -157,9 +141,7 @@ if __name__ == "__main__":
     if os.path.exists(p0) == False:
         os.mkdir(p0, 0o777)
 
-    # p0 = p0 + 'lr' + str(lr) + '/'
-    # if os.path.exists(p0) == False:
-    #     os.mkdir(p0, 0o777)
+  
 
     p0=p0+wavelet_name+'/'
     if os.path.exists(p0) == False:
@@ -183,21 +165,21 @@ if __name__ == "__main__":
         ratio_calculator = nn.DataParallel(ratio_calculator)
         ratio_calculator.to(device)
         
-        # optimizer = optim.Adam(reconstructor.parameters(), 1 * 1e-4,betas=(0.5,0.999))
+     
         params = ([a for a in reconstructor.parameters()] + [b for b in ratio_calculator.parameters()])
         optimizer = optim.RMSprop(params, lr)
 
-        vloss = float('inf')  # 
-        losst = []  # 
+        vloss = float('inf')  
+        losst = []  
 
         lossv = []
 
         ep = -1
         counter = 0
-        total_running = epoches  # 
+        total_running = epoches  
         epoch_train_times = []
-        for epoch in range(epoches):  # 
-            # scheduler.step()  #
+        for epoch in range(epoches):  
+        
             trainloader, val1loader, val2loader, testloader = form_dataloader(batchsize, datapath)
 
             del val2loader
@@ -206,21 +188,20 @@ if __name__ == "__main__":
             torch.cuda.synchronize()
             start = time.clock()
             losst = train(reconstructor, xfm, ifm, device, trainloader, ratio_calculator, epoch, losst, optimizer,
-                          len_list)  #
+                          len_list) 
             del trainloader
             gc.collect()
             torch.cuda.synchronize()
             end = time.clock()
             epoch_train_time = end - start
             epoch_train_times.append(epoch_train_time)
-            # scheduler_g.step()       #
-            # scheduler_d.step()
+     
             lossv, vloss, ep, counter = val(reconstructor, xfm, ifm, window, ratio_calculator, device, val1loader,
                                             epoch, ep, lossv, vloss, counter, patience, threshold,batchsize,len_list, outp, optimizer)
             del val1loader
             gc.collect()
             if counter == patience:
-                total_running = epoch + 1  # 
+                total_running = epoch + 1   
                 break
             torch.cuda.empty_cache()
           
@@ -235,7 +216,7 @@ if __name__ == "__main__":
         del val1loader
         inference(device, val2loader, nc, ts_cin, freq_cout, xfm, ifm, window, ratio_nc, len_list,batchsize, outp, 'val2')
 
-        inference(device, testloader, nc, ts_cin, freq_cout, xfm, ifm, window, ratio_nc, len_list, batchsize,outp, 'test')  # 该次inference时需要重新构造一个model(net)
+        inference(device, testloader, nc, ts_cin, freq_cout, xfm, ifm, window, ratio_nc, len_list, batchsize,outp, 'test')  
 
         data_lt = np.asarray(losst).reshape(len(losst), 1)
         data_lv = np.asarray(lossv).reshape(len(lossv), 1)
